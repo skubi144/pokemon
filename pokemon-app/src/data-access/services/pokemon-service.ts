6295e16754b30efdffd34a8e1be97dd34eb70945
miloszskubala <miloszskubala@gmail.com>
2025-07-12 13:52:57 +0200

    update

--- pokemon-app/src/data-access/services/pokemon-service.ts
+++ pokemon-app/src/data-access/services/pokemon-service.ts
@@ -1,45 +1,60 @@
 import {Injectable} from '@angular/core';
 import {PokemonDetail} from '../model/pokemonDetail';
 import {HttpClient} from '@angular/common/http';
-import {map, Observable} from 'rxjs';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {map, Observable, shareReplay} from 'rxjs';
 import {
   ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner
 } from '../model/apiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class PokemonService {
+  private cache = new Map<string, Observable<unknown>>();
 
   constructor(private http: HttpClient) {
   }
 
-  getPokemon(id: PokemonDetail['id'] | PokemonDetail['name']) {
-    return this.http.get<PokemonDetail>(`/pokemon/${id}`)
+  private getOrCache<T>(key: string, factory: () => Observable<T>): Observable<T> {
+    if (this.cache.has(key)) {
+      return this.cache.get(key)! as Observable<T>;
+    }
+    const observable$ = factory().pipe(shareReplay(1));
+    this.cache.set(key, observable$);
+    return observable$;
+  }
+
+  getPokemon(id: string | number): Observable<PokemonDetail> {
+    return this.getOrCache(`pokemon-${id}`, () =>
+      this.http.get<PokemonDetail>(`/pokemon/${id}`)
+    );
   }
 
   getPokemonAll(): Observable<ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner[]> {
-    return this.http.get<{
-      results: ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner[]
-    }>(`/pokemon?limit=100000&offset=0`).pipe(map(({results}) =>
-      results
-    ));
+    return this.getOrCache('pokemon-all', () =>
+      this.http.get<{
+        results: ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner[]
+      }>(
+        '/pokemon?limit=100000&offset=0'
+      ).pipe(map(({results}) => results))
+    );
   }
 
-  getBerriesAll() {
-    return this.http.get<{
-      results: ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner[]
-    }>(`/berry?limit=100000&offset=0`).pipe(map(({results}) =>
-      results
-    ));
+  getBerriesAll(): Observable<ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner[]> {
+    return this.getOrCache('berries-all', () =>
+      this.http.get<{
+        results: ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner[]
+      }>(
+        '/berry?limit=100000&offset=0'
+      ).pipe(map(({results}) => results))
+    );
   }
 
-  getPotionsAll() {
-    return this.http.get<{
-      items: ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner[]
-    }>(`/item-category/healing?limit=100000&offset=0`).pipe(map(({items}) =>
-      items
-    ));
+  getPotionsAll(): Observable<ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner[]> {
+    return this.getOrCache('potions-all', () =>
+      this.http.get<{
+        items: ApiV2PokemonEncountersRetrieve200ResponseInnerVersionDetailsInnerEncounterDetailsInnerConditionValuesInner[]
+      }>(
+        '/item-category/healing?limit=100000&offset=0'
+      ).pipe(map(({items}) => items))
+    );
   }
 }
